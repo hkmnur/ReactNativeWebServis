f401da689fce03fbc0191b0912a91af02385bd9f
hkmnur <hnerkcan98@gmail.com>
2021-07-27 19:16:21 +0300

    commit

---
+++ WebServis/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { StyleSheet,SafeAreaView,View, Text, Button } from 'react-native';
+
+import axios from 'axios';
+
+export default class App extends Component {
+	state = { /*name surname loading field */
+		name: '',
+		surname: '',
+		loading: true
+	};
+
+	componentDidMount() {
+    this.getRandomUser ();
+	}
+
+  getRandomUser = async() =>{
+    this.setState({
+      loading:true,
+    });
+    const { data: { results } } = await axios.get('https://randomuser.me/api/');
+    const {name: {first,last } } = results[0];
+    
+    this.setState({
+      name:first,
+      surname: last,
+      loading:false
+    });
+		
+  };
+
+	render() {
+		const {name,surname,loading} = this.state;
+		return (
+			<SafeAreaView style={styles.container}>
+				<View>
+					{
+						loading ? <Text style={styles.text}>Loading...</Text> :
+							<Text style={styles.text}>{name} {surname}</Text>
+					}
+          <Button
+          title={"Random User"}
+          onPress={this.getRandomUser}/>
+  			</View>
+			</SafeAreaView>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+  text:{
+    textAlign:'center'
+  }
+});
